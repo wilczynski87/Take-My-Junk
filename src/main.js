a984b53542aad4b69ab775a13dd3ff3aa64493ee
Karol Wilczynski <wilczynski87@gmail.com>
2022-04-05 12:41:16 +0200

    dropping down main menu and auction repaired

--- src/main.js
+++ src/main.js
@@ -6,19 +6,15 @@ import MenuPanel from './menu';
 const auctionsURL = `http://localhost:8081/getAuctionsByConsumerId/1`;
 
 const Main = () => {
-    const [auctions, setAuctions] = useState("dupa zbita");
-    const [context, setUser] = useContext(UserContext);
+    const [auctions, setAuctions] = useState("No active auctions to display");
+    const [clicked, setClicked] = useState(null);
+    const [context, setContext] = useContext(UserContext);
 
-    const fetchBuilder = (myUser, myPassword) => {
-        const rawUrl = "http://localhost:4000/";
-        return "" + rawUrl + myUser + "/" + myPassword;
-    }
-
-    const setAuc = (auc) => {
-        if(auc === "dupa zbita") {
-             //console.log("no auction to display!");
+    const setAuc = (auct) => {
+        if(auct === "No active auctions to display") {
+             return <div className='w3-center'>{clicked === ` w3-hide` ? auctions : null}</div>;
         } else {
-            return auc.map((auction) => <Auction key={auction.id} body={auction} />);
+            return auct.map((auction) =><Auction key={auction.id} body={auction} /> );
         }
     }
 
@@ -29,17 +25,22 @@ const Main = () => {
     useEffect(async () => {
         const auctionByCustomer = await fetch(auctionsURL);
         const auctionJSON = await auctionByCustomer.json();
-        console.log(auctionJSON);
-        setUser({type: 'setAuctions', payload: auctionJSON});
+        setContext({type: 'setAuctions', payload: auctionJSON});
     }, []);
+
+    const show = (click) => {
+        click ===` w3-hide` || null ? setClicked(` w3-show`) : setClicked(` w3-hide`);
+        if(click === null) setAuctions(context.auctions);
+    }
 
     return (
         <div className=''>
-            <div className='w3-panel'>Hello {firstName(context.user.fullName)} </div> <MenuPanel /><br />
-            <div className='w3-card-4' onClick={() => setAuctions(context.auctions)}>
-                Active Auctions
-            {setAuc(auctions)}
+            <div className='w3-panel'>
+                <div className='w3-left'>Hello {firstName(context.user.fullName)}</div> 
+                <div className='w3-right'><MenuPanel /> </div> 
             </div> <br />
+            <div className={`w3-container `} onClick={() => show(clicked)}> Active Auctions </div>
+            <div className={`${clicked}`}> {setAuc(auctions)} </div> <br />
             <div className='w3-card-4'>Ended Auctions</div>
         </div>
 )};
--- src/menu.js
+++ src/menu.js
@@ -22,21 +22,13 @@ const MenuPanel = () => {
     const showMenu = " w3-show";
 
     return (
-        // <div className='w3-round-xxlarge w3-display-topright w3-panel w3-card w3-dropdown-hover'>
-        //     <img src={menuIcon} className="w3-button" alt="menu" />
-        //     <ul className="w3-dropdown-content w3-bar-block w3-card-4 ">
-        //         <li className="w3-bar-item w3-button" onClick={null}>Settings</li>
-        //         <li className="w3-bar-item w3-button" onClick={() => wrapper('main')}>Main</li>
-        //         <li className="w3-bar-item w3-button" onClick={() => wrapper('logout')}>Log out</li>
-        //     </ul>
-        // </div>
-        <div className ="w3-dropdown-click w3-right">
+        <div className ="w3-dropdown-click">
             <button onClick={show} className="w3-button "><img src={menuIcon} className="" alt="menu" /></button>
-            <div id="Demo" className={`w3-dropdown-content w3-bar-block w3-animate-zoom ${ddmenu ? showMenu : null}`}>
-                <a href="#" className="w3-bar-item w3-button">Link 1</a>
-                <a href="#" className="w3-bar-item w3-button">Link 2</a>
-                <a href="#" className="w3-bar-item w3-button">Link 3</a>
-            </div>
+            <ul id="Demo" className={`w3-dropdown-content w3-bar-block w3-animate-zoom ${ddmenu ? showMenu : null}`}>
+                <li href="#" className="w3-bar-item w3-button" onClick={() => wrapper('main')}>Main</li>
+                <li href="#" className="w3-bar-item w3-button" onClick={null}>Settings</li>
+                <li href="#" className="w3-bar-item w3-button" onClick={() => wrapper('logout')}>Log out</li>
+            </ul>
         </div>
     );
 }
